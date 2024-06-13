6db93fba865e6974578c1579ac928d41b558de6a
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 16:46:54 +0900

    [refactoring] extract constant and remove redundant logic

--- similarity.py
+++ similarity.py
@@ -1,12 +1,11 @@
 class SimilarityChecker:
+    LENGTH_MAX_SCORE = 60
     def test(self, obj1, obj2):
         pass
 
     def length_similarity(self, obj1, obj2):
-        if len(obj1) == len(obj2):
-            return 60
 
         longer = obj1 if len(obj1) > len(obj2) else obj2
         shorter = obj2 if len(obj1) > len(obj2) else obj1
 
-        return 60 * (1 - (len(longer) - len(shorter)) / len(longer))
+        return SimilarityChecker.LENGTH_MAX_SCORE * (1 - (len(longer) - len(shorter)) / len(longer))
