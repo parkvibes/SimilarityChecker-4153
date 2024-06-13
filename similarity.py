22952904464c6f14a88176ceaebdf8ae18a847e2
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 16:45:32 +0900

    [feature] add test for same lengths and normal

--- similarity.py
+++ similarity.py
@@ -1,3 +1,12 @@
 class SimilarityChecker:
     def test(self, obj1, obj2):
         pass
+
+    def length_similarity(self, obj1, obj2):
+        if len(obj1) == len(obj2):
+            return 60
+
+        longer = obj1 if len(obj1) > len(obj2) else obj2
+        shorter = obj2 if len(obj1) > len(obj2) else obj1
+
+        return 60 * (1 - (len(longer) - len(shorter)) / len(longer))
--- test_similarity.py
+++ test_similarity.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
+from similarity import SimilarityChecker
 
 
 class TestSimilarityChecker(TestCase):
-    def test_test(self):
-        self.fail()
+    def test_length_similarity(self):
+        sut = SimilarityChecker()
+
+        self.assertEqual(sut.length_similarity("abc", "abc"), 60)
+        self.assertEqual(sut.length_similarity("abce", "bc"), 30)
+
+
+
+
+
+
