6373558b7ac91b7640d0a50652c7994b9d1d5faf
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 17:19:57 +0900

    [feature] add alphabet_similarity

--- similarity.py
+++ similarity.py
@@ -1,3 +1,27 @@
 class SimilarityChecker:
     def test(self, obj1, obj2):
         pass
+
+    def alphabet_similarity(self, obj1, obj2):
+        obj1_alphabet_exist = []
+        obj2_alphabet_exist = []
+
+        for i in range(26):
+            obj1_alphabet_exist.append(False)
+            obj2_alphabet_exist.append(False)
+
+        for i in range(len(obj1)):
+            obj1_alphabet_exist[ord(obj1[i]) - ord('A')] = True
+
+        for i in range(len(obj2)):
+            obj2_alphabet_exist[ord(obj2[i]) - ord('A')] = True
+
+        TotalCnt = sum([1 if obj1_alphabet_exist[i] or obj2_alphabet_exist[i] else 0 for i in range(26)])
+        SameCnt = sum([1 if obj1_alphabet_exist[i] and obj2_alphabet_exist[i] else 0 for i in range(26)])
+
+        return SameCnt / TotalCnt * 40
+
+
+
+
+
--- test_similarity.py
+++ test_similarity.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
+from similarity import SimilarityChecker
 
 
 class TestSimilarityChecker(TestCase):
-    def test_test(self):
-        self.fail()
+    def test_alphabet_similarity(self):
+        a = SimilarityChecker()
+        self.assertEqual(a.alphabet_similarity("ABC", "ABC"), 40)
+        self.assertEqual(a.alphabet_similarity("EFG", "ABC"), 0)
