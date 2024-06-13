fe61c9fc6c457a54e9118b393981e97ebd6d9fed
Hyunwoo Park <dshw.park@samsung.com>
2024-06-13 17:24:30 +0900

    [refactoring] extract function and introduce constant

--- similarity.py
+++ similarity.py
@@ -1,25 +1,33 @@
 class SimilarityChecker:
+    ALPHABET_MAX = 40
     def test(self, obj1, obj2):
         pass
 
     def alphabet_similarity(self, obj1, obj2):
-        obj1_alphabet_exist = []
-        obj2_alphabet_exist = []
+        obj1_alphabet_exist = self.check_alphabet_existence(obj1)
+        obj2_alphabet_exist = self.check_alphabet_existence(obj2)
+
+        TotalCnt = self.get_total_cnt_value(obj1_alphabet_exist, obj2_alphabet_exist)
+        SameCnt = self.get_same_cnt_value(obj1_alphabet_exist, obj2_alphabet_exist)
+
+        return SameCnt / TotalCnt * SimilarityChecker.ALPHABET_MAX
+
+    def get_same_cnt_value(self, obj1_alphabet_exist, obj2_alphabet_exist):
+        return sum([1 if obj1_alphabet_exist[i] and obj2_alphabet_exist[i] else 0 for i in range(26)])
+
+    def get_total_cnt_value(self, obj1_alphabet_exist, obj2_alphabet_exist):
+        return sum([1 if obj1_alphabet_exist[i] or obj2_alphabet_exist[i] else 0 for i in range(26)])
+
+    def check_alphabet_existence(self, obj):
+        ret = []
 
         for i in range(26):
-            obj1_alphabet_exist.append(False)
-            obj2_alphabet_exist.append(False)
+            ret.append(False)
 
-        for i in range(len(obj1)):
-            obj1_alphabet_exist[ord(obj1[i]) - ord('A')] = True
+        for i in range(len(obj)):
+            ret[ord(obj[i]) - ord('A')] = True
 
-        for i in range(len(obj2)):
-            obj2_alphabet_exist[ord(obj2[i]) - ord('A')] = True
-
-        TotalCnt = sum([1 if obj1_alphabet_exist[i] or obj2_alphabet_exist[i] else 0 for i in range(26)])
-        SameCnt = sum([1 if obj1_alphabet_exist[i] and obj2_alphabet_exist[i] else 0 for i in range(26)])
-
-        return SameCnt / TotalCnt * 40
+        return ret
 
 
 
